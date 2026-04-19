c98a08149d565c6c06f8a321964b47f5c2bb82c9
aozodbek2197 <aozodbek2197@gmail.com>
2026-04-19 13:59:33 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// 1-mashq
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n=5;
+    cout<<__builtin_popcount(n);
+}
+// 2-mashq
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n=8;
+    cout<<(n>0 && (n&(n-1))==0);
+}
